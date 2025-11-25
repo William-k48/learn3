8863127d75d97715aa08fca3ff9132cfd2aab031
kamdem william <williamlapense@gmail.com>
2025-11-25 19:23:39 +0100

    lien corriger2

--- learn3/vite.config.ts
+++ learn3/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/learn3/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
